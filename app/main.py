ccd45475b167667a52d8439098eb50c30018bd92
Davy <davytrabalho34@gmail.com>
2026-06-29 23:48:52 -0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 class Person:
-    # write your code here
-    pass
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
+        
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    n_list = [Person(p["name"], p["age"]) for p in people]
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+        if person_data.get("wife"):
+            person.wife = Person.people[person_data["name"]]
+        if person_data.get("husband"):
+            person.husband = Person.people[person_data["husband"]]
+    return n_list
